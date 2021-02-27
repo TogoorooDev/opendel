1535825d1491a3a555ab546761893d57c1cfb7ae
TogoorooDev <robhensley10@gmail.com>
2021-02-26 20:10:29 -0800

    Improved directory deletion

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::fs;
 use std::env;
+use std::io;
+use std::io::Write;
 use glob::glob;
 
 fn del(file: String){
@@ -9,20 +11,33 @@ fn del(file: String){
     }
 }
 
+fn glob_dir(glob_val: String){
+    for entry in glob(&glob_val).unwrap(){
+        del(String::from(entry.unwrap().to_str().unwrap()));
+    }
+}
+
 fn main() {
     let mut args: Vec<String> = env::args().collect();
     args.remove(0);
     for file in args{
         if file.contains("*") {
-            for entry in glob(&file).unwrap(){
-                del(String::from(entry.unwrap().to_str().unwrap()));
-            }
+            glob_dir(file);
         }else {
             let metadata = fs::metadata(&file).unwrap();
             if metadata.is_file(){
                 del(file);
             }else {
-                eprintln!("{}: is a directory", file);
+                // eprintln!("{}: is a directory", file);
+                let mut line = String::new();
+                print!("{} is a directory. Delete {}/*?(Y/N): ", file, file);
+                io::stdout().flush();
+                let _b1 = io::stdin().read_line(&mut line).unwrap();
+                match line.as_str().trim() {
+                    "y" | "Y" => glob_dir(file + "/*"),
+                    "n" | "N" => println!("Not deleting {}/*", file),
+                    _ => println!("Unknown value {}. Not deleting {}/*", line, file)
+                }
             }
         }
        
